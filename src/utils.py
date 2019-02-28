3db3a48b82428823761cb0902eb43cc4da3e9b9a
alex <shaymanov@inbox.ru>
2019-03-01 02:55:27 +0300

    fix config in data processing

--- src/utils.py
+++ src/utils.py
@@ -9,4 +9,4 @@ def str_to_class(module_name, class_name):
             raise AttributeError(f'Class does not exist: {class_name}')
     except ImportError:
         raise ImportError(f'Module does not exist: {module_name}')
-return class_ or None
+    return class_ or None
