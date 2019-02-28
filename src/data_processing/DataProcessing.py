1185f2e5945dc073970f0e9f20b9b0a4e077d8db
alex <shaymanov@inbox.ru>
2019-03-01 02:37:37 +0300

    add files from develop

--- src/data_processing/DataProcessing.py
+++ src/data_processing/DataProcessing.py
@@ -3,66 +3,109 @@ import numpy as np
 
 """
 DataProcessing module.
-
 Date: 16.02.2019
-
 Description:
-
-
-
-
-
-
-
 """
 
-class DataProcessorBase:
 
-    def __init__(self, cell_names=None, **kwargs):
-        self.cell_names = cell_names  # ?
+
+
+
+class DataProcessorBase:
+    """
+    Base class for all the data processing
+    """
+    def __init__(self, column_names=None, suffix=None, **kwargs):
+        """
+        Create instance of data processing object that will process specified columns of the Dataframe
+        :param column_names: list of strings
+        :param suffix: str
+        :param kwargs:
+        """
+        self.column_names = column_names
+        self.suffix = suffix
 
     def load(self, path, key='table', **kwargs):
-
+        """
+        Probably should remove load/save functionality from dp and put it in the Dataframe wrapper
+        :param path:
+        :param key:
+        :param kwargs:
+        :return:
+        """
         # TODO: fix for large frames: (generators?)
 
         return pd.read_hdf(path, key)
 
     def save(self, obj, path, key='table', **kwargs):
+        """
+            Probably should remove load/save functionality from dp and put it in the Dataframe wrapper
+            :param path:
+            :param key:
+            :param kwargs:
+            :return:
+        """
 
         # TODO: fix for large frames: (generators?)
 
         obj.to_hdf(path, key)
 
     def data_loader(self, path, **kargs):
+        """
+        Remove soon
+        :param path:
+        :param kargs:
+        :return:
+        """
         return pd.read_csv(path, dtype={'s': np.int16, 'ttf': np.float32})
 
     def __call__(self, df, *args, **kwargs):
-
+        """
+        Perform processing on the dataframe
+        :param df: Pandas.DataFrame
+        :param args:
+        :param kwargs:
+        :return: modified df: Pandas.Dataframe
+        """
         raise NotImplementedError
-
         return df
 
 
 class DataProcessorMin(DataProcessorBase):
-    def __init__(self, **kwargs):
+    """
+    Calculate running min
+    """
+    def __init__(self, window_length=None, **kwargs):
         super(DataProcessorMin, self).__init__(**kwargs)
-        self.window = kwargs['window_length']  # ?
+        if type(window_length) is not int:
+            raise AttributeError("Window length has to be int")
+        self.window = window_length
+        if not self.suffix:
+            self.suffix = '_min_' + str(self.window)
 
     def __call__(self, df, *args, **kwargs):
-
-        # TODO: fix for large frames: (generators?)
-
-        for name in self.cell_names:
-            df[name + '_min_' + str(self.window)] = df[name].rolling(self.window).min()
+        for name in self.column_names:
+            df[name + self.suffix] = df[name].rolling(self.window, min_periods=1).min()
         return df
 
 
 class DataProcessorMean(DataProcessorBase):
-    def __init__(self, **kwargs):
+    """
+    Calculate running mean
+    """
+    def __init__(self, window_length=None, **kwargs):
+        """
+        :param window_length: int
+        :param kwargs:
+        """
         super(DataProcessorMean, self).__init__(**kwargs)
-        self.window = kwargs['window_length']
+        if type(window_length) is not int:
+            raise AttributeError("Window length has to be int")
+        self.window = window_length
+        if not self.suffix:
+            self.suffix = '_mean_' + str(self.window)
 
     def __call__(self, df, *args, **kwargs):
-        for name in self.cell_names:
-            df[name + '_mean_' + str(self.window)] = df[name].rolling(self.window).mean()
+        for name in self.column_names:
+            df[name + self.suffix] = df[name].rolling(self.window, min_periods=1).mean()
         return df
--- src/data_processing/extract_features.py
+++ src/data_processing/extract_features.py
@@ -4,7 +4,7 @@ import pandas as pd
 import platform
 import matplotlib as mpl
 
-from src.utils import str_to_class
+from utils import str_to_class
 
 if platform.system() == 'Darwin':
     mpl.use('TkAgg')  # Mac OS specific
@@ -28,7 +28,7 @@ def main(**kwargs):
     # 3. Run processing
     print('.......................Processing started.........................')
     for i, p in enumerate(processors):
-        print(f'{i}: name={p.__class__.__name__} | columns={p.cell_names}')
+        print(f'{i}: name={p.__class__.__name__} | columns={p.column_names}')
         df = p(df)
 
     # 4. Save modified dataframe
@@ -41,10 +41,9 @@ def main(**kwargs):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config_fname',
+    parser.add_argument('config_fname',
                         help='name of the config file',
-                        type=str,
-                        default='data_processing_config_example.json')
+                        type=str)
     args = parser.parse_args()
 
     with open(args.config_fname) as config:
