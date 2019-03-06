8c6e4b0ab2fc87385fb0e1edd801e8bbd8ee6399
Pavel <betadecay1993@gmail.com>
2019-03-06 18:58:43 +1100

    Added processing on specific column

--- src/data_processing/DP_feature_calc.py
+++ src/data_processing/DP_feature_calc.py
@@ -35,14 +35,14 @@ def rolling_window(series, window_size, stride, function, params = None):
     return res
 
 
-def calc_data(df, list_of_functions, list_of_params, window_sizes, stride, save_to):
+def calc_data(df,col_name, list_of_functions, list_of_params, window_sizes, stride, save_to, rewrite = True):
     '''
-    Input: pandas array with columns 's' and 'y' 
-    ('s' corresponds to time series and 'y' - to the regression label)
+    Input: pandas array with signal representations
     
-    saves new pandas dataframe in hdf5 extension to "save_to" location
+    Saves new pandas dataframe in hdf5 extension to "save_to" location
     where the columns are the calculated over the time series features from the "list_of_function"
     using windows from "window_sizes" and having a spesified stride
+    "rewrite" specifies wether to discard all the previous information wtitten to the featurized dataframe 
     '''
     
     #checks
@@ -52,41 +52,42 @@ def calc_data(df, list_of_functions, list_of_params, window_sizes, stride, save_
     if stride <=0 :
         raise ValueError("The \"stride\" has to be a postivie number!")
     
-    try:
-        feature_df = pd.read_hdf(save_to) # if there already exists file with features
-    except:
+    if rewrite == False:
+        try:
+            feature_df = pd.read_hdf(save_to) # if there already exists file with features
+        except:
+            feature_df = pd.DataFrame()
+    else:
         feature_df = pd.DataFrame()
     
     num_features = len(list_of_functions)
-    series = np.array(df["s"], dtype = np.float)
+    series = np.array(df[col_name], dtype = np.float)
     for i in range(num_features):
         function = list_of_functions[i].split("*")[0]
         window = window_sizes[i]
         print("Calculating function \"{}\" with params: \"{}\" over the window: \"{}\""\
               .format(function, list_of_params[i], window))
         
-        name_of_col = list_of_functions[i] + "_" + str(window)
-        if name_of_col in list(feature_df.columns):
-            pass
-        else:
+        name_of_new_col = col_name + "_" + list_of_functions[i] + "_" + str(window)
+        if name_of_new_col not in list(feature_df.columns) or rewrite == True:
             res = rolling_window(series, window_sizes[i], stride, function, params = list_of_params[i])
-            feature_df[name_of_col] = res
+            feature_df[name_of_new_col] = res
             
-    feature_df.to_hdf(save_to, key='df')        
+    feature_df.to_hdf(save_to, key='df')
+        
+        
     return None
 
 
 #EXAMPLE
 df = (pd.read_hdf('~./data/sample.h5', key = 'df'))
 df.columns = ["s","y"]
-#if one needs to calculate the same feature twice (althoug with different args) one may use the following syntaxis: function*unique_modifier_1, function**unique_modifier_2
+
 list_of_functions = ["np.max",'np.min', "abs_energy","np.std", \
-                     "quantile*1", "quantile*2", "mean_second_derivative_central"] 
+                     "quantile*1", "quantile*2", "mean_second_derivative_central"]
 list_of_params = [None, None, None, None, {"q" : 0.6}, {"q" : 0.8}, None]
 window_sizes = len(list_of_functions)*[1000]
 
 stride = 500
-save_to = "~./data/featurized_sample.h5"
-calc_data(df, list_of_functions, list_of_params, window_sizes, stride, save_to)
-featurised_df = pd.read_hdf(save_to, key = 'df')
-featurised_df.head()
+save_to = "/home/pavel/Documents/0Research/Projects/LANL-Earthquake/data/featurised_sample.h5"
+calc_data(df, "s", list_of_functions, list_of_params, window_sizes, stride, save_to)
