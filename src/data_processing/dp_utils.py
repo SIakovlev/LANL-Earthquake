96363676918e02d19c2ab51d02bd260da82c2667
Sergey Iakovlev <siakovlev@student.unimelb.edu.au>
2019-03-28 14:30:31 +1100

    SI: add docs to all functions from tsfresh in dp_utils.py

--- src/data_processing/dp_utils.py
+++ src/data_processing/dp_utils.py
@@ -238,138 +238,508 @@ def w_std(df, *args, **kwargs):
 """
 tsfresh routines
 
+- Docs: https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html
+
 """
 
 
 @DumpDecorator
 @WindowDecorator
 def w_abs_energy(df, *args, **kwargs):
+    """
+    Calculates the absolute energy of the time series which is the sum over the squared values
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+    args :
+    kwargs :
+
+    Returns
+    -------
+    The sum over the squared values
+
+    """
     return tsfresh.feature_extraction.feature_calculators.abs_energy(df)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_absolute_sum_of_changes(df, *args, **kwargs):
+    """
+    Returns the sum over the absolute value of consecutive changes in the series
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+    args :
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.absolute_sum_of_changes(df)
 
 
 @DumpDecorator
 @WindowDecorator
+def w_approximate_entropy(df, *args, m=3, r=3, **kwargs):
+    """
+    Implements a vectorized Approximate entropy algorithm.
+
+    https://en.wikipedia.org/wiki/Approximate_entropy
+
+    For short time-series this method is highly dependent on the parameters, but should be stable for N > 2000
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+    args :
+    m : (int) – Length of compared run of data
+    r : (float) – Filtering level, must be positive
+    kwargs :
+
+    Returns
+    -------
+    Approximate entropy of the dataframe
+    """
+    return tsfresh.feature_extraction.feature_calculators.approximate_entropy(df, m, r)
+
+
+@DumpDecorator
+@WindowDecorator
 def w_autocorrelation(df, *args, lag=100, **kwargs):
+    """
+    Calculates the autocorrelation of the specified lag
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+    args :
+    lag : the lag in samples
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.autocorrelation(df, lag=lag)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_binned_entropy(df, *args, max_bins=10, **kwargs):
+    """
+
+    Performs entropy based discretisation
+
+    See description here:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.binned_entropy
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+    args :
+    max_bins :
+    kwargs :
+
+    Returns
+    -------
+
+    """
+
     return tsfresh.feature_extraction.feature_calculators.binned_entropy(df, max_bins=max_bins)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_c3(df, *args, lag=100, **kwargs):
+    """
+    c3 measure was proposed in [1] as a measure of non linearity in the time series.
+
+    For details see:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.c3
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+    args :
+    lag : the lag in samples
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.c3(df, lag=lag)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_count_above_mean(df, *args, **kwargs):
+    """
+    Returns the number of values in x that are higher than the mean of x
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+    args :
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.count_above_mean(df)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_count_below_mean(df, *args, **kwargs):
+    """
+    Returns the number of values in x that are lower than the mean of x
+
+    Parameters
+    ----------
+    df :
+    args :
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.count_below_mean(df)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_first_location_of_maximum(df, *args, **kwargs):
+    """
+    Returns the first location of the maximum value of x. The position is calculated relatively to the length of x.
+
+    Parameters
+    ----------
+    df :
+    args :
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.first_location_of_maximum(df)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_first_location_of_minimum(df, *args, **kwargs):
+    """
+    Returns the first location of the minimal value of x. The position is calculated relatively to the length of x.
+
+    Parameters
+    ----------
+    df :
+    args :
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.first_location_of_minimum(df)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_kurtosis(df, *args, **kwargs):
+    """
+    Returns the kurtosis of x (calculated with the adjusted Fisher-Pearson standardized moment coefficient G2).
+
+    Parameters
+    ----------
+    df :
+    args :
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.kurtosis(df)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_large_standard_deviation(df, *args, r=0.5, **kwargs):
+    """
+    Boolean variable denoting if the standard dev of x is higher than ‘r’ times the range = difference between max and min of x.
+
+    Hence it checks if
+
+    std(x) > r * (max(X)-min(X))
+
+    According to a rule of the thumb, the standard deviation should be a forth of the range of the values.
+
+    Parameters
+    ----------
+    df :
+    args :
+    r : threshold value (see description)
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.large_standard_deviation(df, r=r)
 
 
 @DumpDecorator
 @WindowDecorator
+def w_last_location_of_maximum(df, *args, **kwargs):
+    """
+    Returns the relative last location of the maximum value of x. The position is calculated relatively to the length of x.
+
+    Parameters
+    ----------
+    df :
+    args :
+    kwargs :
+
+    Returns
+    -------
+
+    """
+    return tsfresh.feature_extraction.feature_calculators.last_location_of_maximum(df)
+
+
+@DumpDecorator
+@WindowDecorator
 def w_last_location_of_minimum(df, *args, **kwargs):
+    """
+    Returns the relative last location of the minimum value of x. The position is calculated relatively to the length of x.
+
+    Parameters
+    ----------
+    df :
+    args :
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.last_location_of_minimum(df)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_longest_strike_above_mean(df, *args, **kwargs):
+    """
+    Returns the length of the longest consecutive subsequence in x that is larger than the mean of x
+
+    Parameters
+    ----------
+    df :
+    args :
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.longest_strike_above_mean(df)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_longest_strike_below_mean(df, *args, **kwargs):
+    """
+    Returns the length of the longest consecutive subsequence in x that is smaller than the mean of x
+
+    Parameters
+    ----------
+    df :
+    args :
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.longest_strike_below_mean(df)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_mean_abs_change(df, *args, **kwargs):
+    """
+    Returns the mean over the absolute differences between subsequent time series values which is
+
+    \frac{1}{n} \sum_{i=1,\ldots, n-1} | x_{i+1} - x_{i}|
+
+    Parameters
+    ----------
+    df :
+    args :
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.mean_abs_change(df)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_mean_change(df, *args, **kwargs):
+    """
+    Returns the mean over the differences between subsequent time series values which is
+
+    \frac{1}{n} \sum_{i=1,\ldots, n-1}  x_{i+1} - x_{i}
+
+    Parameters
+    ----------
+    df :
+    args :
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.mean_change(df)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_mean_second_derivative_central(df, *args, **kwargs):
+    """
+    Returns the mean value of a central approximation of the second derivative
+
+    \frac{1}{n} \sum_{i=1,\ldots, n-1}  \frac{1}{2} (x_{i+2} - 2 \cdot x_{i+1} + x_i)
+
+    Parameters
+    ----------
+    df :
+    args :
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.mean_second_derivative_central(df)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_median(df, *args, **kwargs):
+    """
+    Returns the median of x
+
+    Parameters
+    ----------
+    df :
+    args :
+    kwargs :
+
+    Returns
+    -------
+
+    """
+
     return tsfresh.feature_extraction.feature_calculators.median(df)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_number_crossing_m(df, *args, m=0.1, **kwargs):
+    """
+    Calculates the number of crossings of x on m. A crossing is defined as two sequential values where the first value
+    is lower than m and the next is greater, or vice-versa.
+
+    If you set m to zero, you will get the number of zero crossings.
+
+    Parameters
+    ----------
+    df :
+    args :
+    m : threshold value (see description)
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.number_crossing_m(df, m=m)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_number_cwt_peaks(df, *args, n=1, **kwargs):
+    """
+    This feature calculator searches for different peaks in x.
+
+    To do so, x is smoothed by a ricker wavelet and for widths ranging from 1 to n.
+
+    This feature calculator returns the number of peaks that occur at enough width scales and with sufficiently high
+    Signal-to-Noise-Ratio (SNR)
+
+    Parameters
+    ----------
+    df :
+    args :
+    n : max range value (see description)
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.number_cwt_peaks(df, n=n)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_quantile(df, *args, q=0.5, **kwargs):
+    """
+    Calculates the q quantile of x. This is the value of x greater than q% of the ordered values from x.
+
+    Parameters
+    ----------
+    df :
+    args :
+    q (float) : quantile value (see description)
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.quantile(df, q=q)
 
 
 @DumpDecorator
 @WindowDecorator
 def w_ratio_beyond_r_sigma(df, *args, r=0.5, **kwargs):
+    """
+    Ratio of values that are more than r*std(x) (so r sigma) away from the mean of x.
+
+    Parameters
+    ----------
+    df :
+    args :
+    r (float) : multiplier value (see description)
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.ratio_beyond_r_sigma(df, r=r)
 
 # TODO: fix (freezes for some reason)
@@ -381,6 +751,19 @@ def w_ratio_beyond_r_sigma(df, *args, r=0.5, **kwargs):
 @DumpDecorator
 @WindowDecorator
 def w_skewness(df, *args, **kwargs):
+    """
+    Returns the sample skewness of x (calculated with the adjusted Fisher-Pearson standardized moment coefficient G1).
+
+    Parameters
+    ----------
+    df :
+    args :
+    kwargs :
+
+    Returns
+    -------
+
+    """
     return tsfresh.feature_extraction.feature_calculators.skewness(df)
 
 # TODO: fix param r (does not make sense)
@@ -391,7 +774,24 @@ def w_skewness(df, *args, **kwargs):
 
 @DumpDecorator
 @WindowDecorator
-def w_skewness(df, *args, lag=100, **kwargs):
+def w_time_reversal_asymmetry_statistic(df, *args, lag=100, **kwargs):
+    """
+    Boolean variable denoting if the distribution of x looks symmetric. This is the case if
+
+    | mean(X)-median(X)| < r * (max(X)-min(X))
+
+    Parameters
+    ----------
+    df :
+    args :
+    lag : 
+    kwargs :
+
+    Returns
+    -------
+    (bool)
+
+    """
     return tsfresh.feature_extraction.feature_calculators.time_reversal_asymmetry_statistic(df, lag=lag)
 
 
@@ -581,7 +981,7 @@ def w_q31(df, *args, **kwargs):
 
     Parameters
     ----------
-    df :
+    df : pandas DataFrame
     args :
     kwargs :
 
