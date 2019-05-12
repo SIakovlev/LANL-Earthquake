98591d85cb06b9a3df445ce817c4a9fe1c75a559
Sergey Iakovlev <siakovlev@student.unimelb.edu.au>
2019-05-12 22:27:40 +1000

    SI: add script and jupyter file with hyperopt search

--- src/drafts/xgb_hyperopt.py
+++ src/drafts/xgb_hyperopt.py
@@ -6,34 +6,31 @@ from sklearn.metrics import mean_absolute_error
 from xgboost import XGBRegressor
 import pandas as pd
 import numpy as np
-import sys
-sys.path.append('../data_processing/')
-sys.path.append('../../')
-sys.path.append('../../src/')
 
 
-def score(params):
-    print("Training with params: ")
-    print(params)
+def score(params, n=10):
+    print(f"Training with params: {params}")
 
     np.random.seed(0)
     folds = CustomFold(n_splits=9, shuffle=True, fragmentation=0, pad=150)
     loss_list = []
-    for fold_n, (train_index, valid_index) in enumerate(folds.split(train_data)):
-        X_train, X_valid = train_data.iloc[train_index], train_data.iloc[valid_index]
-        y_train, y_valid = y_train_data.iloc[train_index], y_train_data.iloc[valid_index]
+    for _ in range(n):
+        for fold_n, (train_index, valid_index) in enumerate(folds.split(train_data)):
+            X_train, X_valid = train_data.iloc[train_index], train_data.iloc[valid_index]
+            y_train, y_valid = y_train_data.iloc[train_index], y_train_data.iloc[valid_index]
 
-        preprocessor = preprocessing.StandardScaler()
-        preprocessor.fit(X_train)
-        X_train = pd.DataFrame(preprocessor.transform(X_train))
-        X_valid = pd.DataFrame(preprocessor.transform(X_valid))
+            preprocessor = preprocessing.StandardScaler()
+            preprocessor.fit(X_train)
+            X_train = pd.DataFrame(preprocessor.transform(X_train))
+            X_valid = pd.DataFrame(preprocessor.transform(X_valid))
 
-        model = XGBRegressor(**params)
-        model.fit(X_train, y_train)
-        predictions = model.predict(X_valid)
-        loss_list.append(mean_absolute_error(predictions, y_valid))
+            model = XGBRegressor(**params)
+            model.fit(X_train.values, y_train.values)
+            predictions = model.predict(X_valid.values)
+            loss_list.append(mean_absolute_error(predictions, y_valid))
 
     loss = np.mean(loss_list)
+    print(f"Score: {loss}")
     return {'loss': loss, 'status': STATUS_OK}
 
 
@@ -44,9 +41,9 @@ def optimize(random_state=314159265):
     """
 
     space = {
-        'n_estimators': scope.int(hp.quniform('n_estimators', 10, 300, 5)),
-        'eta': hp.quniform('eta', 0.005, 0.5, 0.005),
-        'max_depth': scope.int(hp.quniform('max_depth', 1, 5, 1)),
+        'n_estimators': scope.int(hp.quniform('n_estimators', 10, 50, 1)),
+        'eta': hp.quniform('eta', 0.005, 0.3, 0.005),
+        'max_depth': scope.int(hp.quniform('max_depth', 1, 8, 1)),
         'min_child_weight': scope.int(hp.quniform('min_child_weight', 1, 10, 1)),
         'subsample': hp.quniform('subsample', 0.5, 1, 0.05),
         'gamma': hp.quniform('gamma', 0.5, 1, 0.05),
@@ -69,7 +66,6 @@ def optimize(random_state=314159265):
 if __name__ == '__main__':
 
     df = pd.read_hdf('../../data/e3.h5', key='table')
-
     train_data = df.drop(['ttf'], axis=1)
     y_train_data = df['ttf']
 
