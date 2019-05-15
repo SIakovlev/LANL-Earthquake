1fbfdcedf7c6bc35c917e410ebbf22fdf98f36d7
qdenisq <qdenisq@gmail.com>
2019-05-15 14:40:38 +1000

    DS: add ensemble of mlp (hypothetically smoothens prediction)

--- src/models/mlp_net.py
+++ src/models/mlp_net.py
@@ -5,6 +5,7 @@ import torch
 import torch.utils.data
 import numpy as np
 from models.models import ModelBase
+import pickle
 
 
 def init_weights(m):
@@ -58,11 +59,11 @@ class MLP(Module, ModelBase):
         x = x.view(-1, self.in_features)
         x = self.bn(x)
         x = x.view(orig_shape)
-        x = self.dropout(x)
+        # x = self.dropout(x)
         for linear in self.linears:
             x = self.relu(linear(x))
             x = self.dropout(x)
-        res = self.relu(self.out(x))
+        res = self.out(x)
         return res
 
     def fit(self, train_data, train_y):
@@ -75,7 +76,7 @@ class MLP(Module, ModelBase):
         n_train_steps_per_epoch = train_data.shape[0] // self.minibatch_size
 
         for e in range(self.num_epochs):
-            print(f" epoch: {e} out of {self.num_epochs}")
+            # print(f" epoch: {e} out of {self.num_epochs}")
             for i in range(n_train_steps_per_epoch):
                 batch_idx = np.random.randint(low=0, high=train_data.shape[0], size=self.minibatch_size)
                 x_batch = train_data[batch_idx]
@@ -87,10 +88,9 @@ class MLP(Module, ModelBase):
                 loss.backward()
                 self.optim.step()
 
-                mae_loss = torch.abs(predict - y_batch).mean()
-                print(f"\r step: {i} | mse_loss={loss.detach().cpu():.4f} | mae_loss={mae_loss.detach().cpu():.4f}",
-                      end="")
-            print()
+                # mae_loss = torch.abs(predict - y_batch).mean()
+            #     print(f"\r step: {i} | obj_loss={loss.detach().cpu():.4f}", end="")
+            # print()
         del train_data
         del train_y
 
@@ -100,5 +100,74 @@ class MLP(Module, ModelBase):
         y_predict = self(test_data)
         del test_data
         return y_predict.detach().cpu().numpy()
+
+    def load(f):
+        # with open(fname, 'r') as f:
+
+        net = pickle.load(f)
+        return net
+
+
+class MLPEnsemble(Module, ModelBase):
+    def __init__(self, **kwargs):
+        super(MLPEnsemble, self).__init__()
+
+        self.num_nets = kwargs["num_nets"]
+
+        self.nets = ModuleList([MLP(**kwargs) for _ in range(self.num_nets)])
+
+        self.device = kwargs['device']
+        self.optim = Adam(self.parameters(), lr=kwargs['learning_rate'])
+        self.minibatch_size = kwargs['minibatch_size']
+        self.num_epochs = kwargs['num_epochs']
+        self.loss = L1Loss(reduction='none')
+        self = self.to(self.device)
+
+
+    def forward(self, x):
+        res = torch.stack([net(x) for net in self.nets], dim=-1)
+        return res
+
+    def fit(self, train_data, train_y):
+        self.train()
+
+
+        train_data = torch.tensor(train_data.values.astype(np.float32)).to(self.device)
+        train_y = torch.tensor(train_y.values.astype(np.float32)).view(-1, 1).to(self.device)
+
+        # valid_y = torch.tensor(valid_y.values.astype(np.float32)).view(-1, 1).to(self.device)
+
+        n_train_steps_per_epoch = train_data.shape[0] // self.minibatch_size
+
+        for e in range(self.num_epochs):
+            # print(f" epoch: {e} out of {self.num_epochs}")
+            for i in range(n_train_steps_per_epoch):
+                batch_idx = np.random.randint(low=0, high=train_data.shape[0], size=self.minibatch_size)
+                x_batch = train_data[batch_idx]
+                y_batch = train_y[batch_idx]
+                predict = self(x_batch)
+                self.optim.zero_grad()
+                loss = self.loss(predict[:, :, :], y_batch.repeat(1,1,self.num_nets).transpose(0,1)).mean(dim=(0,1)).sum()
+                loss.backward()
+                self.optim.step()
+
+                # mae_loss = torch.abs(predict - y_batch).mean()
+            #     print(f"\r step: {i} | obj_loss={loss.detach().cpu():.4f}", end="")
+            # print()
+        del train_data
+        del train_y
+
+    def predict(self, test_data):
+        self.eval()
+        test_data = torch.tensor(test_data.values.astype(np.float32)).to(self.device)
+        y_predict = self(test_data).mean(dim=-1)
+        del test_data
+        return y_predict.detach().cpu().numpy()
+
+    def load(f):
+        # with open(fname, 'r') as f:
+
+        net = pickle.load(f)
+        return net
 
 
